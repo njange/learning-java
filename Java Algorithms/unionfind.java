bc044c77338fa5d9c480a14638d9c03d60d55635
Githuo Njange <128843179+njange@users.noreply.github.com>
2024-03-16 23:41:18 +0300

    first commit

--- Java Algorithms/unionfind.java
+++ Java Algorithms/unionfind.java
@@ -1,16 +1,12 @@
-class Main {
-    public static void main(String[] args) {
-        int n = 10;
-        UnionFind uf = new UnionFind(n);
-        uf.union(1, 2);
-        uf.union(2, 3);
-        uf.union(3, 4);
-        uf.union(4, 5);
-        uf.union(5, 6);
-        uf.union(6, 7);
-        uf.union(7, 8);
-        uf.union(8, 9);
-        uf.union(9, 10);
-        System.out.println(uf.find(1, 10));
+public static void mains(String[] args) {
+  int N = StdIn.readInt();
+    UF uf = new UF(N);
+    while (!StdIn.isEmpty()) {
+        int p = StdIn.readInt();
+        int q = StdIn.readInt();
+        if (uf.connected(p, q)) continue;
+        uf.union(p, q);
+        StdOut.println(p + " " + q);
     }
+    uf.union(0, 1);
 }
