fad0cead15d604e96aec098dc402a06dc81e5479
Githuo Njange <128843179+njange@users.noreply.github.com>
2024-03-17 22:06:05 +0300

    quick find algo comments

--- Java Algorithms/quickfind.java
+++ Java Algorithms/quickfind.java
@@ -1,14 +1,22 @@
 public class quickfind {
     private int[] id;
+
+  //Set the id of each object to itself
     public quickfind(int N) {
         id = new int[N];
         for (int i = 0; i < N; i++) {
             id[i] = i;
         }
     }
+
+    //Check whether p and q are in the same component
+
     public boolean connected(int p, int q) {
         return id[p] == id[q];
     }
+
+    //Change all entries with id[p] to id[q]
+    //at most 2N + 2 array accesses
     public void union(int p, int q) {
         int pid = id[p];
         int qid = id[q];
