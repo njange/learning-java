3baf580c489dc6f04ad2b0f5524a390f849fb455
njange <jamesnjange80@gmail.com>
2024-05-24 01:22:31 +0300

    variables

--- Variables.java
+++ Variables.java
@@ -1,7 +1,11 @@
 public class Variables {
     
-        public static void main(String a[]) {
-            System.out.print(3+5);
+        public static void main(String a[]) 
+        {
+            int num1 = 3;
+            int num2 = 5;
+            int result = num1 + num2;
+            System.out.println(result);
         }
     
 }
