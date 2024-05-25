b996b763d394fe87b304bbef132155015ab036ef
njange <jamesnjange80@gmail.com>
2024-05-25 23:58:48 +0300

    relational operators =,<,>,!,==

--- relationaloperators.java
+++ relationaloperators.java
@@ -1,3 +1,10 @@
 public class relationaloperators {
     
+    public static void main(String[] args) {
+        int x = 6;
+        int y = 10;
+        boolean result = x != y;
+
+        System.out.println("Result: " + result);
+    }
 }
