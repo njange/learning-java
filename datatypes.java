e2a4d2ddb75e0408510cdb7ed1a2d3f9eafa7311
njange <jamesnjange80@gmail.com>
2024-05-25 22:44:18 +0300

    floats and integers

--- datatypes.java
+++ datatypes.java
@@ -1,8 +1,20 @@
 public class datatypes {
     public static void main(String a[]) {
 
-        float marks = 6.5;
-        
+        float marks = 6.5f;
+        long population = 7000000000L;
+        short age = 20;
+        byte children = 2;
+
+        System.out.println("Marks: " + marks);
+        System.out.println("Population: " + population);
+        System.out.println("Age: " + age);
+        System.out.println("Children: " + children);    
+
+        float f = 10.5f;
+        double d = 10.5;
+        System.out.println("Float: " + f);
+        System.out.println("Double: " + d);
 
         System.out.println();
     }
