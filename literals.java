baabc0af1b10027d0e1a298ce154ca3de5ac5036
njange <jamesnjange80@gmail.com>
2024-05-25 23:01:01 +0300

    literals

---
+++ literals.java
@@ -0,0 +1,14 @@
+public class literals {
+    public static void main(String[] args) {
+        
+    
+    //literals
+    int num1 = 10_000_000;
+    int num2 = 0b1010;
+
+
+    System.out.println("num1: " + num1);
+    System.out.println("num2: " + num2);
+    }
+
+}
