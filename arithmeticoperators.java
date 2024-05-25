d989f2c17caff9aeba0009f80d3044f09e96ea81
njange <jamesnjange80@gmail.com>
2024-05-25 23:35:57 +0300

    arithmetic operators in java

---
+++ arithmeticoperators.java
@@ -0,0 +1,19 @@
+public class arithmeticoperators {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+
+        int sum = num1 + num2;
+        int diff = num1 - num2;
+        int prod = num1 * num2;
+        int div = num1 / num2;
+        int mod = num1 % num2;
+
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + diff);
+        System.out.println("Product: " + prod);
+        System.out.println("Division: " + div);
+        System.out.println("Modulus: " + mod);
+        
+    }
+}
