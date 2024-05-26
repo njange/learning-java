557cbc7a868d8cc98ea18256eb88ebb9100cb93b
njange <jamesnjange80@gmail.com>
2024-05-26 23:59:55 +0300

    ternary operators

---
+++ ternaryoperator.java
@@ -0,0 +1,15 @@
+public class ternaryoperator {
+    public static void main(String[] args){
+        int n = 4;
+        int result = 0;
+
+        if(n%2 == 0){
+            result = 10;
+        } else {
+            result = 20;
+        }
+
+        System.out.println(result);
+    }
+    
+}
