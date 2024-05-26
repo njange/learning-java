334d6bb42f89e5814ef41df895e1ae4c5a223e7a
njange <jamesnjange80@gmail.com>
2024-05-26 23:25:08 +0300

    logical operators

--- logicaloperators.java
+++ logicaloperators.java
@@ -1,6 +1,19 @@
 public class logicaloperators {
     public static void main(String[] args) {
-        
+        int x = 6;
+        int y = 10;
+
+        boolean result = x > 5 && y < 11;
+        System.out.println("Result: " + result);
+
+        boolean result2 = x > 5 || y < 11;
+        System.out.println("Result2: " + result2);
+
+        boolean result3 = x != y;
+        System.out.println("Result3: " + result3);
+
+        boolean result4 = x == y;
+        System.out.println("Result4: " + result4);
     }
     
 }
