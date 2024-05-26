a1150c33e80a7668d3bbcbad704d3d5f11bfbc0c
njange <jamesnjange80@gmail.com>
2024-05-26 23:37:42 +0300

    if else statements in java

---
+++ ifelse.java
@@ -0,0 +1,15 @@
+
+public class ifelse {
+
+    public static void main(String[] args) {
+        int x = 18;
+
+        if (x > 10) {
+
+            System.out.println("hello");
+        } else {
+            System.out.println("Bye");
+        }
+    }
+
+}
