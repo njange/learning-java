a0826510e044793db11b2c7faf68f9b00f600e28
njange <jamesnjange80@gmail.com>
2024-07-20 12:00:00 +0300

    Update switchstatements.java

--- java basics/switchstatements.java
+++ java basics/switchstatements.java
@@ -1,4 +1,15 @@
 public class switchstatements {
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        int n=1;
+        if (n==1)
+            System.out.println("Sunday");
+        else if (n==2)
+        System.out.println("Monday");
+        System.out.println("Tuesday");
+        System.out.println("Wednesday");
+        System.out.println("Thursday");
+        System.out.println("Friday");
+        System.out.println("Saturday");
+    }
     
 }
